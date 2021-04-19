86faa2be7e0270e3577dc21c3baf63c61ee5b2f7
vicastillo <vicente468@outlook.com>
2021-04-19 11:35:30 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1AnalisisGuia11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1AnalisisGuia11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.Title = "Impresión de los elementos de vectores.";
+
+            Double[] Vectores = new Double[7];
+            for (int i = 0; i < 7; i++)
+            {
+            Console.WriteLine("Ingrese el valor del vector " + (i + 1) + ":");
+            Vectores[i] = Double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Los vectores ingresados son:");
+            for (int i = 0; i < 7; i++)
+            {
+            Console.Write("\t" + Vectores[i]);
+            }
+
+            }
+            catch (Exception error)
+            {
+            Console.WriteLine("Error: " + error.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
